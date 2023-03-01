447f19efebbf8569115666ed7e6f4131e5bdd80c
SixGryphon <nilton_tadeu@yahoo.com.br>
2023-02-28 22:52:04 -0400

    feat: changed theme to nightfox

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,12 +73,11 @@ require('lazy').setup({
       },
     },
   },
-
-  { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+  { -- Theme nightfox
+    'EdenEast/nightfox.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'nightfox'
     end,
   },
 
@@ -88,7 +87,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'nightfox',
         component_separators = '|',
         section_separators = '',
       },
